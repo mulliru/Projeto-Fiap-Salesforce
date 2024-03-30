0e873c8c4e6a0fc7dd6e7788b0392a27163c3226
Murillo Ferreira <murilloferreira348@gmail.com>
2024-03-30 12:21:44 -0300

    Atualizando a entrega de banco

--- Terceira Entregas/1TDSPB_2024_DataBase/Entrega_Sprint_3/1TDSPB_2024_CodigoSql.sql
+++ Terceira Entregas/1TDSPB_2024_DataBase/Entrega_Sprint_3/1TDSPB_2024_CodigoSql.sql
@@ -12,8 +12,6 @@ Pedro Luiz Prado            RM: 553874
 '''
 
 DROP TABLE produto
-DROP TABLE funcionario
-DROP TABLE empresa
 DROP TABLE formulario_usuario_cadastro
 DROP TABLE usuario
 DROP TABLE pedido
@@ -27,25 +25,6 @@ CREATE TABLE produto (
     valor_produto FLOAT NOT NULL
 );
 
-CREATE TABLE funcionario(
-    id_funcionario INTEGER NOT NULL PRIMARY KEY,
-    cpf_funcionario CHAR (11) NOT NULL, 
-    nome_funcionario VARCHAR(100) NOT NULL,
-    sobrenome_funcionario VARCHAR(100) NOT NULL,
-    setor_funcionario CHAR (1),
-    salario_funcionario FLOAT
-);
-
-CREATE TABLE empresa(
-    cnpj_empresa CHAR (14) NOT NULL PRIMARY KEY,
-    nome_empresa VARCHAR (100) NOT NULL,
-    status_empresa CHAR (1) NOT NULL CHECK (status_empresa IN ('I', 'A', 'F')),
-    endereco_empresa VARCHAR (100),
-    telefone_empresa VARCHAR (15),
-    --Chaves que são referenciadas de outras tabelas
-    id_funcionario INTEGER REFERENCES funcionario (id_funcionario),
-    id_produto INTEGER REFERENCES produto (id_produto)
-);
 
 CREATE TABLE formulario_usuario_cadastro (
     id_fr_usuario INTEGER NOT NULL PRIMARY KEY,
@@ -57,14 +36,14 @@ CREATE TABLE formulario_usuario_cadastro (
 );
 
 CREATE TABLE usuario (
-    id_usuario VARCHAR(100) NOT NULL PRIMARY KEY,
+    id_usuario INTEGER NOT NULL PRIMARY KEY,
     apelido_usuario VARCHAR(100) NOT NULL,
     nome_usuario VARCHAR(100) NOT NULL,
     sobrenome_usuario VARCHAR(100) NOT NULL,
     email_usuario VARCHAR(100) NOT NULL,
     senha_usuario VARCHAR(256) NOT NULL,
     --Chaves que são referenciadas de outras tabelas
-    id_fr_usuario INTEGER REFERENCES formulario_usuario_cadastro (id_usuario)
+    id_fr_usuario INTEGER REFERENCES formulario_usuario_cadastro (id_fr_usuario)
 );
 
 CREATE TABLE pedido (
@@ -72,12 +51,10 @@ CREATE TABLE pedido (
     data_pedido DATE,
     status_pedido VARCHAR(3) CHECK (status_pedido IN ('PEN', 'ENV', 'ENT', 'CAN')),
     valor_pedido FLOAT,
-    --Chaves que são referenciadas de outras tabelas
+    --Chaves estrangeiras corrigidas
     id_funcionario INTEGER REFERENCES funcionario (id_funcionario),
     id_usuario INTEGER REFERENCES usuario (id_usuario)
 );
-
-
 
 CREATE TABLE endereco (
     id_end INTEGER NOT NULL PRIMARY KEY,
@@ -95,13 +72,6 @@ CREATE TABLE contato (
     telefone_contato VARCHAR(15) NOT NULL,
     email_contato VARCHAR(100) NOT NULL,
     --Chaves que são referenciadas de outras tabelas
-    id_usuario VARCHAR (100) REFERENCES usuario (id_usuario)
+    id_usuario INTEGER REFERENCES usuario (id_usuario)
 
 );
-
-
-
-
-
-
-
