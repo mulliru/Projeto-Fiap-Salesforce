e583c54d56380d291cdb645f86ec009e59e2f53a
Murillo Ferreira <murilloferreira348@gmail.com>
2024-04-14 19:36:58 -0300

    Fazendo insert, uptade e delete no banco de dados

--- Quarta Entregas/1TDSPB_2024_DataBase/script_atualizado.sql
+++ Quarta Entregas/1TDSPB_2024_DataBase/script_atualizado.sql
@@ -17,6 +17,7 @@ DROP TABLE usuario
 DROP TABLE pedido
 DROP TABLE endereco
 DROP TABLE contato
+Drop TABLE produto_pedido
 
 CREATE TABLE produto (
     id_produto INTEGER NOT NULL PRIMARY KEY,
@@ -85,3 +86,64 @@ CREATE TABLE contato (
     id_usuario INTEGER REFERENCES usuario (id_usuario)
 
 );
+
+--Fazendo os inserts
+
+
+--Formato de datas é: 'YYYY-MM-DD' ou seja ano/mês/dia
+
+-- Fazendo Insert de usuarios no formulario
+INSERT INTO formulario_usuario_cadastro (id_fr_usuario, cpf_fr_usuario, nome_fr_usuario, sobrenome_fr_usuario, dt_nascimento_fr_usuario, email_fr_usuario, senha_fr_usuario) 
+VALUES 
+(1, '12345678901', 'Murillo', 'Ramos', '1999-12-12', 'murilloemail@fiap.com.br', '123456'),
+(2, '98081500049', 'Pedro', 'Prado', '2004-12-23', 'pedroluiz@fiap.com.br', '789102'),
+(3, '17728866028', 'Keven', 'Ike', '1999-12-12', 'kevenike@fiap.com.br', '456789'),
+(4, '23225536032', 'Lucas', 'Oliveira', '1999-18-12', 'lucas@fiap.com.br', '789456');
+
+
+-- Fazendo Insert de usuarios
+
+
+-- Fazendo Insert de produtos
+INSERT INTO produto (id_produto, nome_produto, tipo_produto, valor_produto)
+VALUES
+  (1, 'Sales Cloud Essentials', 'CRM', 25),
+  (2, 'Sales Cloud Professional', 'CRM', 80),
+  (3, 'Sales Cloud Enterprise', 'CRM', 165),
+  (4, 'Sales Cloud Unlimited', 'CRM', 330),
+  (5, 'Service Cloud Essentials', 'CRM', 25),
+  (6, 'Service Cloud Professional', 'CRM', 80),
+  (7, 'Service Cloud Enterprise', 'CRM', 165),
+  (8, 'Service Cloud Unlimited', 'CRM', 330),
+  (9, 'Marketing Cloud Email Marketing', 'Marketing', 150),
+  (10, 'Marketing Cloud Marketing Automation', 'Marketing', 600),
+  (11, 'Marketing Cloud Social Marketing', 'Marketing', 1200),
+  (12, 'Marketing Cloud Einstein Marketing Advertising', 'Marketing', 3000),
+  (13, 'Commerce Cloud Essentials', 'Comércio Eletrônico', 25),
+  (14, 'Commerce Cloud Professional', 'Comércio Eletrônico', 80),
+  (15, 'Commerce Cloud Enterprise', 'Comércio Eletrônico', 165),
+  (16, 'Commerce Cloud Unlimited', 'Comércio Eletrônico', 330);
+
+select * from produto;
+select * from formulario_usuario_cadastro;
+
+
+-- Fazendo update de dados --
+
+-- Fazendo update de usuarios no formulario
+UPDATE formulario_usuario_cadastro SET sobrenome_fr_usuario = 'Ferreira' WHERE id_fr_usuario = 1;
+-- Fazendo update de usuarios
+UPDATE usuario set apelido_usuario = 'murilloramos' where id_usuario = 1;
+-- Fazendo update de produtos
+UPDATE produto SET valor_produto = 30 WHERE id_produto = 1;
+
+-- Fazendo delete de dados --
+
+-- Fazendo delete de usuarios no formulario
+DELETE FROM formulario_usuario_cadastro WHERE id_fr_usuario = 4;
+
+-- Fazendo delete de usuarios #FALTA COLOCAR USUARIO NA TABELA DE USUARIO
+ DELETE FROM usuario WHERE id_usuario =
+
+-- Fazendo delete de produtos
+DELETE FROM produto WHERE id_produto = 16;
